99d1a901b7efb9983b7882c5492e0e88aed6b864
oks767 <aleksandrovao767oks@yandex.ru>
2024-01-12 07:22:30 +0300

    added slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Slider.css';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 const Slider = () => {
   return (
     <div className='slider__wrapper'>
@@ -8,26 +12,47 @@ const Slider = () => {
         <h1 className='slider__title'>
           Акции и подарки <span>для каждой семьи</span>
         </h1>
-        <div className='slider__cards'>
-          <div className='card slider-img-one'>
-            <h3>Любая перепланировка в подарок!</h3>
-            <p>
-              Рыбатекст используется дизайнерами, проектировщиками и
-              фронтендерами, когда нужно выстро заполнить макеты или прототипы
-              содержимым.
-            </p>
-            <button className='slider__button'>перейти к акции</button>
-          </div>
-          <div className='card slider-img-two'>
-            <h3>Металлическая дверь в подарок!</h3>
-            <p>
-              Рыбатекст используется дизайнерами, проектировщиками и
-              фронтендерами, когда нужно выстро заполнить макеты или прототипы
-              содержимым.
-            </p>
-            <button className='slider__button'>перейти к акции</button>
-          </div>
-        </div>
+        <Swiper
+          modules={[Navigation, Pagination]}
+          slidesPerView={2}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          <SwiperSlide>
+            <div className='card slider-img-one'>
+              <h3>Любая перепланировка в подарок!</h3>
+              <p>
+                Рыбатекст используется дизайнерами, проектировщиками и
+                фронтендерами, когда нужно выстро заполнить макеты или прототипы
+                содержимым.
+              </p>
+              <button className='slider__button'>перейти к акции</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='card slider-img-two'>
+              <h3>Металлическая дверь в подарок!</h3>
+              <p>
+                Рыбатекст используется дизайнерами, проектировщиками и
+                фронтендерами, когда нужно выстро заполнить макеты или прототипы
+                содержимым.
+              </p>
+              <button className='slider__button'>перейти к акции</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='card slider-img-two'>
+              <h3>Металлическая дверь в подарок!</h3>
+              <p>
+                Рыбатекст используется дизайнерами, проектировщиками и
+                фронтендерами, когда нужно выстро заполнить макеты или прототипы
+                содержимым.
+              </p>
+              <button className='slider__button'>перейти к акции</button>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className='slider__cards'></div>
       </div>
     </div>
   );
